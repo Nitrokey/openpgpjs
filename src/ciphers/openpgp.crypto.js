ee610e6bc7f772a74eea3b5a442983d07b13519f
Peter Pentchev <roam@ringlet.net>
2013-08-22 17:31:25 +0300

    Move the generic WebCrypto funcs where they belong.

--- src/ciphers/openpgp.crypto.js
+++ src/ciphers/openpgp.crypto.js
@@ -241,61 +241,6 @@ function openpgp_crypto_signData_own(algo, key, message) {
 }
 
 /**
- * Create a signature on data using the specified algorithm
- * @param {Integer} hash_algo hash Algorithm to use (See RFC4880 9.4)
- * @param {Integer} algo Asymmetric cipher algorithm to use (See RFC4880 9.1)
- * @param {openpgp_type_mpi[]} publicMPIs Public key multiprecision integers 
- * of the private key 
- * @param {WebCrypto.Key} privateKey Private key used to sign the data
- * @param {String} data Data to be signed
- * @return {openpgp_promise} signed data (string)
- */
-function openpgp_crypto_signData(hash_algo, algo, publicMPIs, privateKey, data) {
-	var res = new openpgp_promise();
-	var toMPI = false;
-	
-	// FIXME: honor hash_algo, too :)
-
-	var algorithm;
-
-	switch(algo) {
-	case 1: // RSA (Encrypt or Sign) [HAC]  
-	case 2: // RSA Encrypt-Only [HAC]
-	case 3: // RSA Sign-Only [HAC]
-		algorithm = { name: 'RSASSA-PKCS1-v1_5', params: { hash: 'SHA-256' } };
-		toMPI = true;
-		break;
-	case 17: // DSA (Digital Signature Algorithm) [FIPS186] [HAC]
-		algorithm = { name: 'ECDSA', hash: { name: 'SHA-256' } };
-		break;
-	case 16: // Elgamal (Encrypt-Only) [ELGAMAL] [HAC]
-		res._onerror("signing with Elgamal is not defined in the OpenPGP standard.");
-		return res;
-	default:
-		res._onerror("unknown OpenPGP signing algorithm " + algo);
-		return res;
-	}	
-
-	var sign = openpgp_webcrypto_subtle.sign(algorithm, privateKey, util.str2Uint8Array(data));
-	sign.oncomplete = function (e) {
-		var r;
-		if (!toMPI) {
-			r = e.target.result;
-		} else {
-			var s = util.hexidump(e.target.result);
-			var bi = new BigInteger(s, 16);
-			r = bi.toMPI();
-		}
-		res._oncomplete(r);
-	}
-	sign.onerror = function (e) {
-		res._onerror(e.target.result);
-	}
-
-	return res;
-}
-
-/**
  * Create a hash on the specified data using the specified algorithm
  * @param {Integer} algo Hash algorithm type (see RFC4880 9.4)
  * @param {String} data Data to be hashed
@@ -442,38 +387,6 @@ function openpgp_crypto_testRSA(key){
 	var result = rsa.decrypt(msg, key.d, key.p, key.q, key.u);
 }
 
-/**
- * @typedef {Object} openpgp_keypair
- * @property {WebCrypto.Key} privateKey 
- * @property {WebCrypto.Key} publicKey
- * @property {String} publicKeyArmored
- */
-
-function openpgp_keypair()
-{
-	this.privateKey = null;
-	this.publicKey = null;
-	this.publicKeyArmored = null;
-}
-
-/**
- * @typedef {Object} openpgp_keypair_raw
- * @property {Integer} numBits
- * @property {Integer} symmetricEncryptionAlgorithm
- * @property {String} timePacket
- * @property {WebCrypto.Key} privateKey
- * @property {WebCrypto.Key} publicKey
- */
-
-function openpgp_keypair_raw()
-{
-	this.numBits = null;
-	this.symmetricEncryptionAlgorithm = null;
-	this.timePacket = null;
-	this.privateKey = null;
-	this.publicKey = null;
-}
-
 function openpgp_crypto_stashKey_own(pair, numBits){
 	/**
 	 * OK, so this kind of "hopes" that no one imports keys from
@@ -521,24 +434,25 @@ function openpgp_crypto_stashKey_own(pair, numBits){
 }
 
 function openpgp_crypto_pair_from_RSA(key, numBits, algo, privKeyUsage, publicKeyUsage) {
-	return {
-		privateKey: {
-			type: 'private',
-			extractable: true,
-			algorithm: algo,
-			keyUsage: privKeyUsage,
-			opgp_key: key,
-			opgp_numBits: numBits
-		},
-		publicKey: {
-			type: 'public',
-			extractable: true,
-			algorithm: algo,
-			keyUsage: publicKeyUsage,
-			opgp_key: key,
-			opgp_numBits: numBits
-		}
+	var pair = new openpgp_keypair();
+
+	pair.privateKey = {
+		type: 'private',
+		extractable: true,
+		algorithm: algo,
+		keyUsage: privKeyUsage,
+		opgp_key: key,
+		opgp_numBits: numBits
 	};
+	pair.publicKey = {
+		type: 'public',
+		extractable: true,
+		algorithm: algo,
+		keyUsage: publicKeyUsage,
+		opgp_key: key,
+		opgp_numBits: numBits
+	};
+	return pair;
 }
 
 function openpgp_crypto_digKey_own(algo, numBits) {
@@ -621,75 +535,6 @@ function openpgp_crypto_generateKeyPair_own(algo, numBits, passphrase, s2kHash, 
 	}
 
 	res._oncomplete(pair);
-	return res;
-}
-
-function openpgp_crypto_generateKeyPair(keyType, numBits, symmetricEncryptionAlgorithm)
-{
-	var d = new Date();
-	d = d.getTime()/1000;
-	var timePacket = String.fromCharCode(Math.floor(d/0x1000000%0x100)) + String.fromCharCode(Math.floor(d/0x10000%0x100)) + String.fromCharCode(Math.floor(d/0x100%0x100)) + String.fromCharCode(Math.floor(d%0x100));
-
-	var res = new openpgp_promise();
-	var algo;
-	
-	switch (keyType) {
-	case 1:
-		algo = {
-			name: 'RSASSA-PKCS1-v1_5',
-			params: {
-				modulusLength: numBits,
-				publicExponent: new Uint8Array([0x01, 0x00, 0x01])
-			}
-		};
-		break;
-	
-	default:
-		res._onerror('Unknown key type ' + keyType);
-		return res;
-	}
-
-	openpgp_webcrypto_subtle.generateKey(algo, false, ["sign"]).then(
-		function (key) {
-			switch (keyType) {
-			case 1:
-				var keyPair = new openpgp_keypair_raw();
-
-				keyPair.numBits = numBits;
-				keyPair.publicKey = key.target.result.publicKey;
-				keyPair.privateKey = key.target.result.privateKey;
-				keyPair.symmetricEncryptionAlgorithm = symmetricEncryptionAlgorithm;
-				keyPair.timePacket = timePacket;
-				res._oncomplete(keyPair);
-				break;
-			default:
-				res._onerror("We shouldn't have reached generateKeyPair.gen.oncomplete() with an unknown key type " + keyType);
-				break;
-			}
-		},
-		function (e) {
-			res._onerror(e.target.result);
-		}
-	);
-
-	return res;
-}
-
-function openpgp_crypto_exportKey(format, key) {
-	var res = new openpgp_promise();
-
-	openpgp_webcrypto_subtle.exportKey(format, key).then(
-		function (e) {
-			try {
-				res._oncomplete(e.target.result);
-			} catch (err) {
-				// FIXME: Bah, we don't really need this level of detail
-				res._onerror("openpgp_crypto_exportKey.res._oncomplete failed: " + err);
-			}
-		},
-		function (e) {
-			res._onerror(e.target.result);
-		});
 	return res;
 }
 
